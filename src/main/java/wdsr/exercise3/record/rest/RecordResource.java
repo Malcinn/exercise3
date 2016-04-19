a5755b28e0aaeda26d95fd14edd09c917f056792
Malcinn <marcinkowalczyk1992@gmail.com>
2016-04-19 19:26:38 +0200

    restserver finished.

--- src/main/java/wdsr/exercise3/record/rest/RecordResource.java
+++ src/main/java/wdsr/exercise3/record/rest/RecordResource.java
@@ -1,11 +1,31 @@
 package wdsr.exercise3.record.rest;
 
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.GenericEntity;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import wdsr.exercise3.record.Record;
 import wdsr.exercise3.record.RecordInventory;
 
 @Path("/records")
@@ -28,6 +48,13 @@ public class RecordResource {
 	 * ** Returns a list of all records (as application/xml)
 	 * ** Response status: HTTP 200
 	 */
+	@GET
+	@Produces(MediaType.APPLICATION_XML)
+	public Response getRecords(){
+		List<Record> results = this.recordInventory.getRecords();
+		GenericEntity<List<Record>> genericResults = new GenericEntity<List<Record>>(results) {};
+		return Response.ok(genericResults).build();
+	}
 
 	/**
 	 * POST https://localhost:8091/records
@@ -36,6 +63,26 @@ public class RecordResource {
 	 * ** Response status if ok: HTTP 201, Location header points to the newly created resource.
 	 * ** Response status if submitted record has ID set: HTTP 400
 	 */
+	@POST
+	@Consumes(MediaType.APPLICATION_XML)
+	public Response createRecord(Record record, @Context UriInfo uriInfo){
+		if (record.getId() == null){
+			int recordId = 0;
+			UriBuilder uriBuilder;
+			try{
+				 recordId = this.recordInventory.addRecord(record);
+			} catch(IllegalArgumentException e){
+				return Response.status(Status.BAD_REQUEST).entity(e.getMessage()).build();
+			}
+			uriBuilder = uriInfo.getAbsolutePathBuilder();
+			uriBuilder.path(Integer.toString(recordId));
+			return Response.created(uriBuilder.build()).build();
+		} else {
+			return Response.status(Status.BAD_REQUEST).build();
+		}
+	}
+	
+	
 	
 	/**
 	 * * GET https://localhost:8091/records/{id}
@@ -43,6 +90,17 @@ public class RecordResource {
 	 * ** Response status if ok: HTTP 200
 	 * ** Response status if {id} is not known: HTTP 404
 	 */
+	@GET
+	@Path("/{id}")
+	@Produces(MediaType.APPLICATION_XML)
+	public Response getRecord(@PathParam(value = "id") int id){
+		Record record = this.recordInventory.getRecord(id);
+		if (record != null){
+			return Response.status(Status.OK).entity(record).build();
+		} else {
+			return Response.status(Status.NOT_FOUND).build();
+		}
+	}
 	 /**
 	 * * PUT https://localhost:8091/records/{id}
 	 * ** Replaces an existing record in entirety.
@@ -50,6 +108,22 @@ public class RecordResource {
 	 * ** Response status if ok: HTTP 204
 	 * ** Response status if {id} is not known: HTTP 404
 	 */
+	@PUT
+	@Path("/{id}")
+	@Consumes(MediaType.APPLICATION_XML)
+	public Response putRecord(Record record, @PathParam(value = "id") int id){
+		if (record.getId() == null || record.getId().equals(id))
+			record.setId(id);
+		else
+			return Response.status(Status.BAD_REQUEST).build();
+		
+		if (this.recordInventory.updateRecord(id, record)){
+			return Response.status(Status.NO_CONTENT).build();
+		} else {
+			return Response.status(Status.NOT_FOUND).build();
+		}
+		
+	}
 	
 	/**
 	 * * DELETE https://localhost:8091/records/{id}
@@ -57,4 +131,13 @@ public class RecordResource {
 	 * ** Response status if ok: HTTP 204
 	 * ** Response status if {id} is not known: HTTP 404
 	 */
+	@DELETE
+	@Path("/{id}")
+	public Response deleteRecord(@PathParam("id") int id){
+		if (this.recordInventory.deleteRecord(id)){
+			return Response.status(Status.NO_CONTENT).build();
+		} else {
+			return Response.status(Status.NOT_FOUND).build();
+		}
+	}
 }
